3f3150c951fa939191c5db06386a89aae3340d6c
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-07-13 23:05:14 +0600

    Add component to handle form events.

--- src/Components/TodosLogic.jsx
+++ src/Components/TodosLogic.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import InputTodo from './InputTodo';
 import TodosList from './TodosList';
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: 'Setup development environment',
       completed: false,
     },
     {
-      id: 2,
+      id: uuidv4(),
       title: 'Develop website and add content',
       completed: false,
     },
     {
-      id: 3,
+      id: uuidv4(),
       title: 'Deploy to live server',
       completed: false,
     },
@@ -37,11 +38,25 @@ const TodosLogic = () => {
       ...todos.filter((todo) => todo.id !== id),
     ]);
   };
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
 
   return (
     <div>
-      <InputTodo />
-      <TodosList todosProps={todos} handleChange={handleChange} delTodo={delTodo} />
+      <InputTodo
+        addTodoItem={addTodoItem}
+      />
+      <TodosList
+        todosProps={todos}
+        handleChange={handleChange}
+        delTodo={delTodo}
+      />
     </div>
   );
 };
